124e6e262a54a23d9b203077a20ffb8fe35145a8
farrelekaputra <128167479+farrelekaputra@users.noreply.github.com>
2025-11-11 07:46:31 +0700

    benerin struktur kata sama link

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = ({title}) => {
     return(
         <nav className="bg-white flex justify-center z-50 w-screen -ms-0 fixed top-0 md:p-4 md:px-44 shadow-md">
           <div className="content-center bg-white w-full flex items-center justify-between -ms-6 md:ms-0 mx-auto px-10 md:px-10 sticky top-0">
-            <a href="#" className="w-1/2 md:w-auto flex items-center space-x-3 rtl:space-x-reverse justify-start">
+            <a href="https://blogpararel.vercel.app" className="w-1/2 md:w-auto flex items-center space-x-3 rtl:space-x-reverse justify-start">
               <span className="self-center hidden md:block text-xl font-semibold whitespace-nowrap text-gray-700">{title}</span>
               <span className="self-center md:hidden text-xl font-semibold whitespace-nowrap text-gray-700">Mini Blog</span>
             </a>
